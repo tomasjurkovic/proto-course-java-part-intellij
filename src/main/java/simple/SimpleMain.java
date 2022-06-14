25e899886bc1f2414be765a7954175bddda236d9
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2022-06-14 10:34:24 +0200

    creating a simple object
    creating a simple object by using generated protofile simple.proto and java class

---
+++ src/main/java/simple/SimpleMain.java
@@ -0,0 +1,22 @@
+package simple;
+
+import example.simple.SimpleOuterClass;
+
+import java.util.Arrays;
+
+public class SimpleMain {
+
+    public static void main(String[] args) {
+        SimpleOuterClass.Simple message = SimpleOuterClass.Simple.newBuilder()
+                .setId(45)
+                .setIsSimple(true)
+                .setName("Tomas")
+                .addSimpleList(1)
+                .addSimpleList(2)
+                .addSimpleList(3)
+                .addAllSimpleList(Arrays.asList(4, 5, 6))
+                .build();
+
+        System.out.println(message);
+    }
+}
